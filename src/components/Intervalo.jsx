0bce64f8de355d24288819d2a89097bb665c8bb5
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-06-08 11:22:00 -0300

    action creator #03

--- src/components/Intervalo.jsx
+++ src/components/Intervalo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 import Card from "./Card";
-import { alterarNumeroMinimo } from "../store/actions/numeros";
+import { alterarNumeroMinimo, alterarNumeroMaximo } from "../store/actions/numeros";
 
 function Intervalo(props) {
   const { min, max } = props;
@@ -16,7 +16,7 @@ function Intervalo(props) {
         </span>
         <span>
           <strong>Máximo: </strong>
-          <input type="number" value={max} readOnly />
+          <input type="number" value={max} onChange={e => props.alterarMaximo(+e.target.value)} />
         </span>
       </div>
     </Card>
@@ -34,6 +34,11 @@ function mapDispatchToProps(dispatch) {
     alterarMinimo(novoNumero) {
       //action creator -> action
       const action = alterarNumeroMinimo(novoNumero);
+      dispatch(action);
+    },
+    alterarMaximo(novoNumero) {
+      //action creator -> action
+      const action = alterarNumeroMaximo(novoNumero);
       dispatch(action);
     },
   };
--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,32 +1,32 @@
-import { legacy_createStore as createStore, combineReducers } from 'redux'
-
+import { legacy_createStore as createStore, combineReducers } from "redux";
 
 const reducers = combineReducers({
-    numeros: function(state, action) {
-
-        switch(action.type) {
-            case 'NUM_MIN_ALTERADO':
-                return {
-                    ...state,
-                    min: action.payload
-                } 
-            default: 
-                return {
-                    min: 7,
-                    max: 31
-                }
-        }
-
-    },
-    nomes: function(state, action) {
-        return [
-            'Amanda', 'Carol', 'Ana', 'Helloiza'
-        ]
+  numeros: function (state, action) {
+    switch (action.type) {
+      case "NUM_MIN_ALTERADO":
+        return {
+          ...state,
+          min: action.payload,
+        };
+      case "NUM_MAX_ALTERADO":
+        return {
+          ...state,
+          max: action.payload,
+        };
+      default:
+        return {
+          min: 7,
+          max: 31,
+        };
     }
-})
+  },
+  nomes: function (state, action) {
+    return ["Amanda", "Carol", "Ana", "Helloiza"];
+  },
+});
 
 function storeConfig() {
-    return createStore(reducers)
+  return createStore(reducers);
 }
 
-export default storeConfig
+export default storeConfig;
