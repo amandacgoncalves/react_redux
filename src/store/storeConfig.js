3f6bc2a95a1a85f5e5f797794a7cd396af849350
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-06-08 11:27:45 -0300

    refactoring

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,28 +1,8 @@
 import { legacy_createStore as createStore, combineReducers } from "redux";
+import numerosReducer from "./reducers/numeros";
 
 const reducers = combineReducers({
-  numeros: function (state, action) {
-    switch (action.type) {
-      case "NUM_MIN_ALTERADO":
-        return {
-          ...state,
-          min: action.payload,
-        };
-      case "NUM_MAX_ALTERADO":
-        return {
-          ...state,
-          max: action.payload,
-        };
-      default:
-        return {
-          min: 7,
-          max: 31,
-        };
-    }
-  },
-  nomes: function (state, action) {
-    return ["Amanda", "Carol", "Ana", "Helloiza"];
-  },
+    numeros: numerosReducer
 });
 
 function storeConfig() {
